2c6dddfaf8737ced3523a47945d5b0b34145eeec
divyanshu0x16 <divyanshu22.dm@gmail.com>
2023-06-09 15:35:35 +0530

    able to create users now on the backend

--- server/controllers/AuthController.js
+++ server/controllers/AuthController.js
@@ -1,40 +1,28 @@
-// AuthController.js
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
-exports.signup = (req, res) => {
-  const { username, password } = req.body;
+exports.signup = async (req, res) => {
+  try {
+    const { username, password } = req.body;
 
-  User.findOne({ username }, (err, existingUser) => {
-    if (err) {
-      return res.status(500).json({ message: 'An error occurred' });
-    }
-
+    const existingUser = await User.findOne({ username });
     if (existingUser) {
       return res.status(400).json({ message: 'Username already exists' });
     }
 
-    // Hash the password
-    bcrypt.hash(password, 10, (err, hashedPassword) => {
-      if (err) {
-        return res.status(500).json({ message: 'Error hashing password' });
-      }
-
-      // Create a new user
-      const newUser = new User({
-        username,
-        password: hashedPassword,
-      });
-
-      newUser.save((err, savedUser) => {
-        if (err) {
-          return res.status(500).json({ message: 'Error creating user' });
-        }
-
-        const token = jwt.sign({ username }, 'secretKey');
-        res.status(200).json({ token });
-      });
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({
+      username,
+      password: hashedPassword,
     });
-  });
+
+    const savedUser = await newUser.save();
+
+    const token = jwt.sign({ username }, process.env.SECRET);
+    res.status(200).json({ token, username: savedUser.username });
+  } catch (error) {
+    console.error('Error creating user:', error);
+    res.status(500).json({ message: 'Error creating user' });
+  }
 };
--- server/index.js
+++ server/index.js
@@ -1,10 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const port = 8000;
+
+app.use(express.json());
+require('dotenv').config();
 
 const authRoutes = require('./routes/authRoutes');
 app.use(authRoutes);
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+// Connection URI for MongoDB Atlas cluster
+const uri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTER_URL}`;
+
+mongoose
+  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to the database');
+
+    // Start the server
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error('Error connecting to the database:', error);
+  });
